45673dcb3afdd661808db3c6ac8add1d189c8e20
Abdo <ba2662003@gmail.com>
2024-06-22 14:30:44 +0300

    update in title

--- components/HomeCom/titleOfCom.tsx
+++ components/HomeCom/titleOfCom.tsx
@@ -1,4 +1,6 @@
-import Link from "next/link";
+"use client"
+
+import { useRouter } from "next/navigation";
 
 interface IProp{
     title:string,
@@ -6,11 +8,12 @@ interface IProp{
 }
 
 const TitleOfCom = ({title,view}:IProp) => {
+    const router = useRouter()
     return (
         <div className="flex pb-2 relative justify-between items-center border-b border-b-gray-500
         after:w-20 after:h-1 after:bg-red-600 after:absolute after:bottom-0 after:left-0">
             <h1 className="font-bold">{title}</h1>
-            <Link href={`/Home/viewMore/${view}`} className="uppercase px-2 py-1 font-bold text-xs bg-slate-800 text-white rounded">view more</Link>
+            <button onClick={()=>router.push(`/Home/viewMore/${view}`)} className="uppercase px-2 py-1 font-bold text-xs bg-slate-800 text-white rounded">view more</button>
         </div>
     );
 }
